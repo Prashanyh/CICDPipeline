fec3eba18c656e65114cfdf3eeb94d78b8be8295
Chandana315 <84147654+Chandana315@users.noreply.github.com>
2022-01-05 15:55:26 +0530

    Copied reset url

--- XT01/urls.py
+++ XT01/urls.py
@@ -24,6 +24,7 @@ from UserAdministration import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('UserAdministration.urls')),
+    path('reset/<uidb64>/<token>/',views.PasswordTokenCheckApiView.as_view(),name='password_reset_confirm'), # verifying token
 
     # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
